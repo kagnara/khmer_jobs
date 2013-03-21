ff2955dc01dcc11af57d7e66a41994edb9cbfe9b
kagnara <kol.kagnara@gmail.com>
2013-03-21 18:25:46 +0700

    a

--- module/User/src/User/Controller/UserController.php
+++ module/User/src/User/Controller/UserController.php
@@ -2,11 +2,23 @@
 namespace User\Controller;
 use \Zend\Mvc\Controller\AbstractActionController;
 use \Zend\View\Model\ViewModel;
-
+use \User\Form\UserdataForm;
+use \User\Model\Userdata;
 class UserController extends AbstractActionController {
     function index(){
-        $this->headScript()->appendFile('http://platform.linkedin.com/in.js');
-        return new viewModel();
+       
+            $this->headScript()->appendFile('http://platform.linkedin.com/in.js');
+            
+   
+        $form = new UserdataForm();
+        $form->get('submit')->setValue('Add');
+        $request = $this->getRequest();
+        if($request->isPost()){
+            $Userdata = new Userdata();
+            $Userdata->exchangeArray($form->getData());
+            $this->getUserdataTable()->addtoDb($Userdata);
+        }
+        return array('form'=>$form);
     }
 }
 ?>
